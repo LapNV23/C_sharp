e7f8736901b2af9ff1264626766a442bd5ce631a
LapNV23 <73330679+LapNV23@users.noreply.github.com>
2021-06-27 20:41:24 +0700

    Task3

---
+++ Lab1/BT1_Lab1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BT1_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Lap";
+            string address = "Thai Binh";
+            double phone = 0398408820;
+            Console.WriteLine(name);
+            Console.WriteLine(address);
+            Console.WriteLine(phone);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab1/BT2_Lab1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BT2_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = 10;
+            int numberTwo = 20;
+            int numberThree = 30;
+            Console.WriteLine(Math.Max(numberTwo,numberThree));
+        }
+    }
+}
